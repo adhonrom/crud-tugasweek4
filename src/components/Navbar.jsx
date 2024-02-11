2afc994c61efd1b5a6c18593422c489c8057fc82
Adhon's MacBook <m@Adhons-MacBook-Pro.local>
2024-02-11 17:55:51 +0700

    navbar edit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,23 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
 
-export default function Navbar () {
+export default function Navbar() {
   return (
-    
-    <div >
-        <div className="flex justify-between items-center rounded-lg bg-black px-8 py-3">
-        <img src="/icon.png" height={"30"} width={"30"} alt="logodev " href={"/"}/>
-        <Link className="text-white px-2 font-thin" href={"/"}>DevScale Learning Journey.</Link>
+    <div>
+      <div className="flex justify-between items-center rounded-lg bg-black px-8 py-3">
+        <Link href={"/"}>
+          <img src="/icon.png" height={"30"} width={"30"} alt="logodev" />
+        </Link>
+        <Link className="text-white px-2 font-medium" href={"/"}>
+          DevScale Learning Journey..
+        </Link>
 
-        <Link className="bg-white p-2 justify-end font-thin rounded-md" href={"/addTopic"}>Add Topic</Link>
-        </div >
-        
+        <Link
+          className="bg-white p-2 justify-end rounded-md font-medium text-black hover:bg-black border hover:border-white hover:text-white"
+          href={"/addTopic"}
+        >
+          ➕ Add Topic
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
